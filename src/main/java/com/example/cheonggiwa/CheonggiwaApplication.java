cd0973c5620821149557e3a746a9ec8efcf4aca1
pyb <wowong123@naver.com>
2025-09-20 15:18:10 +0900

    first commit

---
+++ src/main/java/com/example/cheonggiwa/CheonggiwaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheonggiwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheonggiwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheonggiwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cheonggiwa/CheonggiwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheonggiwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheonggiwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
